fd8bd440c171c54ef9fd50f5364a8f391476aacf
Theree <83382093+Turqee@users.noreply.github.com>
2023-03-27 17:14:32 -0500

    THBOT WEBSITE REVAMP V2
    - made the ui a LOT cleaner
    - thats it :)

--- website_stuff/javascript/main.js
+++ website_stuff/javascript/main.js
@@ -5,6 +5,7 @@
     d = n.getDate();
     document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
 
+
     const btn = document.querySelector(".btn-toggle");
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
@@ -30,4 +31,39 @@
       localStorage.setItem("theme", theme);
     });
 
+
+    function openNav() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+    }
   
+    /* DO NOT TOUCH - last updated api ty https://stackoverflow.com/questions/56279807/is-it-possible-to-automatically-have-the-last-updated-date-on-my-website-changed*/
+    const desiredRepo = "seachel.github.io";
+    const dateTagClass = ".date";
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function()
+    {
+      if (this.readyState == 4 && this.status == 200)
+      {
+        let repos = JSON.parse(this.responseText);
+    
+        repos.forEach((repo)=>{
+          if (repo.name == desiredRepo)
+          {
+            var lastUpdated = new Date(repo.updated_at);
+            var day = lastUpdated.getUTCDate();
+            var month = lastUpdated.getUTCMonth();
+            var year = lastUpdated.getUTCFullYear();
+            $(dateTagClass).text(`Last updated: ${year}-${month}-${day}`);
+          }
+        });
+      }
+    };
+    xhttp.open("GET", "https://api.github.com/users/Turqee/repos", true);
+    xhttp.send();
